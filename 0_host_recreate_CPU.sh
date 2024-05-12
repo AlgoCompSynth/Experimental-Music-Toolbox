d43d86891433b5e9725beebc641e77bb90f9cdc3
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2024-05-12 01:42:03 -0700

    refactored creation scripts

--- 0_host_recreate_CPU.sh
+++ 0_host_recreate_CPU.sh
@@ -3,11 +3,34 @@
 set -e
 
 export COMPUTE_MODE=CPU
+echo ""
 echo "COMPUTE_MODE: $COMPUTE_MODE"
+echo ""
+echo "Setting environment variables"
+source ./set_dbx_envars
+set | grep "^DBX"
+echo ""
 
-echo "(Re)creating distrobox"
-distrobox assemble create --replace --file distrobox-$COMPUTE_MODE.ini
+echo "Removing any existing $DBX_CONTAINER_NAME"
+distrobox rm --force $DBX_CONTAINER_NAME
 
-echo "Entering EMT-Bookworm-$COMPUTE_MODE"
+echo "Removing any existing $DBX_CONTAINER_DIRECTORY"
+rm -rf $DBX_CONTAINER_DIRECTORY
+
+echo "Pulling $DBX_CONTAINER_IMAGE"
+podman pull $DBX_CONTAINER_IMAGE
+
+echo "Creating distrobox $DBX_CONTAINER_NAME"
+distrobox create \
+  --image $DBX_CONTAINER_IMAGE \
+  --name $DBX_CONTAINER_NAME \
+  --hostname $DBX_CONTAINER_HOSTNAME \
+  --pull \
+  --home $DBX_CONTAINER_DIRECTORY \
+  --additional-packages "systemd libpam-systemd apt-file file git-lfs lsb-release pciutils plocate software-properties-common time tree vim-nox" \
+  --additional-packages "fftw-dev fftw-docs libfftw3-bin libfftw3-dev libfftw3-doc libfftw3-mpi-dev libopenblas64-pthread-dev" \
+  --init
+  
+echo "Entering $DBX_CONTAINER_NAME"
 echo "Installing the basic packages will take some time"
-distrobox enter "EMT-Bookworm-$COMPUTE_MODE"
+distrobox enter "$DBX_CONTAINER_NAME"
