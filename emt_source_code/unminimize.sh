ec4f791ed434272ccf2d04737f0fffcff151e6eb
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2024-05-16 16:58:49 -0700

    add some more guidance for the user

--- emt_source_code/unminimize.sh
+++ emt_source_code/unminimize.sh
@@ -2,6 +2,18 @@
 
 set -e
 
+echo ""
+echo "Testing man pages..."
+echo "If you see a man page for 'bash', you do not need to run 'unminimize'."
+echo ""
+echo ""
+sleep 5
+man bash
+echo "If you saw a man page for 'bash', you do not need to run 'unminimize'."
+echo "If you got a message saying the system is minimized,"
+echo "you can exit with CTL-C during the next 20 second sleep,"
+echo "or let this script proceed and answer 'y'."
+sleep 20
 echo "Setting empty list of excludes"
 sudo touch /etc/dpkg/dpkg.cfg.d/excludes
 
