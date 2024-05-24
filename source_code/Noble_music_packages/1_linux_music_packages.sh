f3acbabe8d2ea72ff988885ba844c2ee9a528652
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2024-05-24 15:43:14 -0700

    Testing complete on Bookworm distrobox

--- source_code/Noble_music_packages/1_linux_music_packages.sh
+++ source_code/Noble_music_packages/1_linux_music_packages.sh
@@ -5,7 +5,7 @@ set -e
 echo "Defining LOGFILE"
 mkdir --parents Logs
 touch Logs/.gitkeep
-export LOGFILE=Logs/1_linux_music_packages.log
+export LOGFILE=Logs/1_ubuntu_music_packages.log
 
 echo ""
 echo ""
