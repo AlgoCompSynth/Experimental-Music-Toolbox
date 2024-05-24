6f9af4114af71bb469f32a86e597d7426f86d007
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2024-05-23 23:42:09 -0700

    tested in Bookworm distrobox

--- source_code/miniAudicle_from_source/1_install_miniaudicle.sh
+++ source_code/miniAudicle_from_source/1_install_miniaudicle.sh
@@ -7,9 +7,9 @@ export QT_SELECT=qt6
 echo "Setting ChucK version"
 export CHUCK_VERSION="chuck-1.5.2.4"
 
-echo "Clearing Logs"
+echo "Defining LOGFILE"
 mkdir --parents Logs
-rm -f Logs/*
+touch Logs/.gitkeep
 export LOGFILE=$PWD/Logs/1_install_miniaudicle.log
 
 echo "Installing build dependencies"
