9b59eb0afeb500472995d4265e9ad5d4cefd4a69
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2025-03-25 13:25:41 -0700

    Use R from distro!!

--- source_code/RStudio_Server/1_R_and_Quarto_CLI.sh
+++ source_code/RStudio_Server/1_R_and_Quarto_CLI.sh
@@ -5,19 +5,6 @@ set -e
 echo "Defining LOGFILE"
 export LOGFILE=$PWD/Logs/1_R_and_Quarto_CLI.log
 
-echo "Adding CRAN repository"
-# https://cran.rstudio.com/bin/linux/ubuntu/
-
-# add the signing key (by Michael Rutter) for these repos
-# To verify key, run gpg --show-keys /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc
-# Fingerprint: E298A3A825C0D65DFD57CBB651716619E084DAB9
-wget -qO- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc \
-  | sudo tee -a /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc
-# add the R 4.0 repo from CRAN -- adjust 'focal' to 'groovy' or 'bionic' as needed
-sudo add-apt-repository --yes \
-  "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran40/" \
-  >> $LOGFILE 2>&1
-
 echo ""
 echo "Installing R"
 /usr/bin/time sudo apt-get update \
