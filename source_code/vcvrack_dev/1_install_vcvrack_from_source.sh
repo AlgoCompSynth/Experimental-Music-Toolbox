47a1b1b31b4345146071ee4b8a6e220af2435dc5
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2025-03-26 00:35:48 -0700

    VCVRack development environment

---
+++ source_code/vcvrack_dev/1_install_vcvrack_from_source.sh
@@ -0,0 +1,57 @@
+#! /usr/bin/env bash
+
+set -e
+
+echo "Defining LOGFILE"
+mkdir --parents $PWD/Logs
+export LOGFILE=$PWD/Logs/1_install_vcvrack_from_source.log
+rm --force $LOGFILE
+
+echo "Installing build dependencies"
+sudo apt-get update -qq
+/usr/bin/time sudo apt-get upgrade --yes \
+  >> $LOGFILE 2>&1
+/usr/bin/time sudo apt-get install --yes \
+  cmake \
+  curl \
+  gdb \
+  git \
+  jq \
+  libasound2-dev \
+  libglu1-mesa-dev \
+  libgtk2.0-dev \
+  libjack-jackd2-dev \
+  libpulse-dev \
+  libx11-dev \
+  libxcursor-dev \
+  libxi-dev zlib1g-dev \
+  libxinerama-dev \
+  libxrandr-dev \
+  >> $LOGFILE 2>&1
+
+mkdir --parents $HOME/Projects
+pushd $HOME/Projects
+  echo "Cloning repository"
+  rm -fr Rack
+  /usr/bin/time git clone \
+    git@github.com:AlgoCompSynth/Rack.git \
+    >> $LOGFILE 2>&1
+
+  pushd Rack
+    echo "Fetching submodules"
+    /usr/bin/time git submodule update --init --recursive \
+      >> $LOGFILE 2>&1
+
+    echo "make dep"
+    /usr/bin/time make dep \
+      >> $LOGFILE 2>&1
+
+    echo "make"
+    /usr/bin/time make --jobs=`nproc` \
+      >> $LOGFILE 2>&1
+
+  popd
+
+popd
+
+echo "Finished"
