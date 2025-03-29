64d3033478d03465676eba452f32cbbf739e5ef6
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2025-03-29 14:06:22 -0700

    switch to Debian testing!

--- source_code/0_host_recreate_container.sh
+++ source_code/0_host_recreate_container.sh
@@ -49,7 +49,7 @@ then
     --hostname $DBX_CONTAINER_HOSTNAME \
     --pull \
     --home $DBX_CONTAINER_DIRECTORY \
-    --additional-packages "systemd libpam-systemd software-properties-common time vim" \
+    --additional-packages "systemd libpam-systemd time vim" \
     --init
 else
   distrobox create \
@@ -58,7 +58,7 @@ else
     --hostname $DBX_CONTAINER_HOSTNAME \
     --pull \
     --home $DBX_CONTAINER_DIRECTORY \
-    --additional-packages "systemd libpam-systemd software-properties-common time vim" \
+    --additional-packages "systemd libpam-systemd time vim" \
     --init
 fi
   
